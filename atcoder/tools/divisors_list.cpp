434d63245b5924df041a24396ba7f085ba8141a8
solareenlo <solareenlo@gmail.com>
2020-05-20 00:48:25 +0900

    約数のリスト

--- atcoder/tools/divisors_list.cpp
+++ atcoder/tools/divisors_list.cpp
@@ -1,19 +1,18 @@
 // 約数のリストを取得する
 #include <algorithm>
 #include <iostream>
-#include <vector>
+#include <set>
 using namespace std;
 
-vector<int> divisorsList(int n) {
-    vector<int> div;
+set<int> divisorsList(int n) {
+    set<int> div;
     for (int i = 1; i * i <= n; ++i) {
         if (n % i == 0) {
-            div.push_back(i);
+            div.insert(i);
             if (i * i != n)
-                div.push_back(n / i);
+                div.insert(n / i);
         }
     }
-    sort(div.begin(), div.end());
     return div;
 }
 
@@ -21,7 +20,7 @@ int main() {
     int a;
     cin >> a;
 
-    vector<int> div = divisorsList(a);
+    set<int> div = divisorsList(a);
 
     for (int n : div)
         cout << n << ' ';
