22bffe588d7c44d7228702928fb9e1ca934fb19b
solareenlo <solareenlo@gmail.com>
2020-05-20 07:37:54 +0900

    add abc083

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc083/D/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+void solve(std::string S){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    std::string S;
+    std::cin >> S;
+    solve(S);
+    return 0;
+}
