fade6de89029e16023589796514870b9e5c5efd5
solareenlo <solareenlo@gmail.com>
2020-05-23 07:47:33 +0900

    fix gcd

--- atcoder/tools/gcd.cpp
+++ atcoder/tools/gcd.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-inline int gcd(int a, int b){
-    if(b == 0) return a;
+inline int gcd(int a, int b) {
+    if (b == 0) return a;
     return  gcd(b, a % b);
 }
