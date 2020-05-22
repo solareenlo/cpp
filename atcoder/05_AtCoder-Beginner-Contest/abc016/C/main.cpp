6ac0d2d3f309a2fbd04807f60881ccbc52fd0fb4
solareenlo <solareenlo@gmail.com>
2020-05-23 00:01:58 +0900

    add abc016_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc016/C/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+// 出力
+void print() { std::cout << '\n'; }
+template <class T>void print(const T &x) {std::cout << x <<'\n';}
+template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
+
+
+void solve(long long N, long long M, std::vector<long long> A, std::vector<long long> B){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long N;
+    scanf("%lld",&N);
+    long long M;
+    scanf("%lld",&M);
+    std::vector<long long> A(M);
+    std::vector<long long> B(M);
+    for(int i = 0 ; i < M ; i++){
+        scanf("%lld",&A[i]);
+        scanf("%lld",&B[i]);
+    }
+    solve(N, M, std::move(A), std::move(B));
+    return 0;
+}
