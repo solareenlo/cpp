be25f21977ae29d54944b968ffa35c02c85b7dfb
solareenlo <solareenlo@gmail.com>
2020-05-23 07:47:41 +0900

    fix lcm

--- atcoder/tools/lcm.cpp
+++ atcoder/tools/lcm.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 
-inline int gcd(int a, int b){
-    if(b == 0) return a;
+inline int gcd(int a, int b) {
+    if (b == 0) return a;
     return  gcd(b, a % b);
 }
 
-inline int lcm(int a, int b){
-    int g = gcd(a,b);
-    return a / g * b;
+inline int lcm(int a, int b) {
+    return a * b / gcd(a, b);
 }
