9d13feda2cf00c616aafc8cc8ab7a56166ca9532
solareenlo <solareenlo@gmail.com>
2020-05-25 04:32:52 +0900

    add abc110_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc110/C/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+const string YES = "Yes";
+const string NO = "No";
+
+void solve(std::string S, std::string T){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    std::string S;
+    std::cin >> S;
+    std::string T;
+    std::cin >> T;
+    solve(S, T);
+    return 0;
+}
