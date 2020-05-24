f4daa411d6495f7790a1350a72e7fa5ffeaca37d
solareenlo <solareenlo@gmail.com>
2020-05-24 15:24:20 +0900

    add abc097_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc097/C/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(std::string s, long long K){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    std::string s;
+    std::cin >> s;
+    long long K;
+    scanf("%lld",&K);
+    solve(s, K);
+    return 0;
+}
