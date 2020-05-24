2a74642e459ff41ad99f464cc5594475b977842f
solareenlo <solareenlo@gmail.com>
2020-05-25 02:21:57 +0900

    add abc106

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc106/C/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(long long S, long long K){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long S;
+    scanf("%lld",&S);
+    long long K;
+    scanf("%lld",&K);
+    solve(S, K);
+    return 0;
+}
