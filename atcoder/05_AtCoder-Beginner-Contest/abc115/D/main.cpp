2f268a5c03eb3121ac661ea58e3978c65e36e245
solareenlo <solareenlo@gmail.com>
2020-05-25 06:30:33 +0900

    add abc115_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc115/D/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(long long N, long long X){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long N;
+    scanf("%lld",&N);
+    long long X;
+    scanf("%lld",&X);
+    solve(N, X);
+    return 0;
+}
