fcef3f2239c0a0a6097e881726c0f6804da92863
solareenlo <solareenlo@gmail.com>
2020-05-25 15:10:52 +0900

    add abc130_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc130/C/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(long long W, long long H, long long x, long long y){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long W;
+    scanf("%lld",&W);
+    long long H;
+    scanf("%lld",&H);
+    long long x;
+    scanf("%lld",&x);
+    long long y;
+    scanf("%lld",&y);
+    solve(W, H, x, y);
+    return 0;
+}
