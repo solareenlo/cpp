b1397af251484e3008a6116969239cea93b0ed40
solareenlo <solareenlo@gmail.com>
2020-05-26 06:41:13 +0900

    add abc145_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc145/D/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+const long long MOD = 1000000007;
+
+void solve(long long X, long long Y){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long X;
+    scanf("%lld",&X);
+    long long Y;
+    scanf("%lld",&Y);
+    solve(X, Y);
+    return 0;
+}
