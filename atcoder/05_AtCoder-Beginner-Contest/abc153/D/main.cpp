95dc4047f12f46c3184b68b55f2ed05f5e9b82ea
solareenlo <solareenlo@gmail.com>
2020-05-26 07:25:57 +0900

    add abc153_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc153/D/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(long long H){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long H;
+    scanf("%lld",&H);
+    solve(H);
+    return 0;
+}
