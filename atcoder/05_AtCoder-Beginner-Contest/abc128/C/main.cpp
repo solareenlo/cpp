459e5cd738698b44fefe42e5a1e1ae209ff8cf8f
solareenlo <solareenlo@gmail.com>
2020-05-25 14:39:06 +0900

    add abc128

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc128/C/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+const long long MOD = 2;
+
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    // Failed to predict input format
+    return 0;
+}
