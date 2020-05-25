29dedbf8b5946b1daf25064efb2a53cbc9e5e761
solareenlo <solareenlo@gmail.com>
2020-05-26 07:17:41 +0900

    add abc149_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc149/D/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(long long N, long long K, long long R, long long S, long long P, std::string T){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long N;
+    scanf("%lld",&N);
+    long long K;
+    scanf("%lld",&K);
+    long long R;
+    scanf("%lld",&R);
+    long long S;
+    scanf("%lld",&S);
+    long long P;
+    scanf("%lld",&P);
+    std::string T;
+    std::cin >> T;
+    solve(N, K, R, S, P, T);
+    return 0;
+}
