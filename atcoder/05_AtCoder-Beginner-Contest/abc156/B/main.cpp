c77f3f5488ecb9858cbca0fb9aa9e5304043376c
solareenlo <solareenlo@gmail.com>
2020-05-26 07:43:21 +0900

    update abc156_b

--- atcoder/05_AtCoder-Beginner-Contest/abc156/B/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc156/B/main.cpp
@@ -1,26 +1,19 @@
 #include <iostream>
-#include <cmath>
-#define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-void solve(long long N, long long K){
-    int ans = 0;
-    REP(i, 31) {
-        if (pow(K, i) > N) {
-            ans = i;
-            break ;
-        }
-    }
-    cout << ans << '\n';
-}
-
-int main(){
+int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long K;
-    scanf("%lld",&K);
-    solve(N, K);
+
+    int n, k;
+    cin >> n >> k;
+
+    int count = 0;
+    while (n) {
+        n /= k;
+        count++;
+    }
+
+    cout << count << '\n';
     return 0;
 }
