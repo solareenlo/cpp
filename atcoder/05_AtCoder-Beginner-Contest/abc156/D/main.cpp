5844f615f466e9ff9411bead5aec0c98b9f1eadc
solareenlo <solareenlo@gmail.com>
2020-05-26 07:38:07 +0900

    add abc156_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc156/D/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+const long long MOD = 1000000007;
+
+void solve(long long n, long long a, long long b){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long n;
+    scanf("%lld",&n);
+    long long a;
+    scanf("%lld",&a);
+    long long b;
+    scanf("%lld",&b);
+    solve(n, a, b);
+    return 0;
+}
