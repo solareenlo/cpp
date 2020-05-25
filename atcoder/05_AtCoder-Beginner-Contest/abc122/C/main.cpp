6dc9b449720005ed0a3d2155f9ef3cb3608b486d
solareenlo <solareenlo@gmail.com>
2020-05-25 11:58:44 +0900

    add abc122_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc122/C/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(long long N, long long Q, std::string S, std::vector<long long> l, std::vector<long long> r){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long N;
+    scanf("%lld",&N);
+    long long Q;
+    scanf("%lld",&Q);
+    std::string S;
+    std::cin >> S;
+    std::vector<long long> l(Q);
+    std::vector<long long> r(Q);
+    for(int i = 0 ; i < Q ; i++){
+        scanf("%lld",&l[i]);
+        scanf("%lld",&r[i]);
+    }
+    solve(N, Q, S, std::move(l), std::move(r));
+    return 0;
+}
