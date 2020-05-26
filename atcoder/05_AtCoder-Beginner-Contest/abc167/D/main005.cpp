4705407bec571d29162157158d3257f93b225912
solareenlo <solareenlo@gmail.com>
2020-05-27 07:37:49 +0900

    update abc167_d

--- atcoder/05_AtCoder-Beginner-Contest/abc167/D/main005.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc167/D/main005.cpp
@@ -2,19 +2,20 @@
 #include <vector>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
+using ll = long long;
 
 int main() {
     int n;
-    long long k;
+    ll k;
     cin >> n >> k;
 
-    vector<int> a(n); // ワープ手順全部
+    vector<int> a(n);
     REP(i, n) cin >> a[i];
 
-    vector<int> road; // 必要なワープ手順 (tail と loop の部分)
-    vector<int> count(n + 1, -1); // index の街までのワープする街の個数
-    int tail = 0; // tail 部分の街の個数
-    int loop = 1; // loop 部分の街の個数
+    vector<int> road;
+    vector<int> count(n + 1, -1);
+    int tail = 0;
+    int loop = 1;
 
     int v = 1;
     while (count[v] == -1) {
@@ -22,7 +23,7 @@ int main() {
         road.push_back(v);
         v = a[v - 1];
     }
-    tail = count[v]; // v はループ1歩手前の場所
+    tail = count[v];
     loop = road.size() - tail;
 
     if (k < tail) cout << road[k] << '\n';
