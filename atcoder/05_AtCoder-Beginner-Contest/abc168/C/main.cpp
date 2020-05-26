31b003a17c6baaa2db8f7469da8e9a516fd30202
solareenlo <solareenlo@gmail.com>
2020-05-26 18:09:20 +0900

    update abc168_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc168/C/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(long long A, long long B, long long H, long long M){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long A;
+    scanf("%lld",&A);
+    long long B;
+    scanf("%lld",&B);
+    long long H;
+    scanf("%lld",&H);
+    long long M;
+    scanf("%lld",&M);
+    solve(A, B, H, M);
+    return 0;
+}
---
+++ atcoder/05_AtCoder-Beginner-Contest/abc168/D/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+const string YES = "Yes";
+
+void solve(long long N, long long M, std::vector<long long> A, std::vector<long long> B){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long N;
+    scanf("%lld",&N);
+    long long M;
+    scanf("%lld",&M);
+    std::vector<long long> A(M);
+    std::vector<long long> B(M);
+    for(int i = 0 ; i < M ; i++){
+        scanf("%lld",&A[i]);
+        scanf("%lld",&B[i]);
+    }
+    solve(N, M, std::move(A), std::move(B));
+    return 0;
+}
