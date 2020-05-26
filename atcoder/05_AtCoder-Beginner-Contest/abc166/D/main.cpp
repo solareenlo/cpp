638083636fe616329169561bfef19abd7749f7c5
solareenlo <solareenlo@gmail.com>
2020-05-26 17:44:56 +0900

    update abc166_a_b

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc166/D/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+
+void solve(long long X){
+
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    long long X;
+    scanf("%lld",&X);
+    solve(X);
+    return 0;
+}
