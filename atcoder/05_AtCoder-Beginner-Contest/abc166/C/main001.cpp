9f1824fb646e13c0c6faabe0a0e4769872ba4eab
solareenlo <solareenlo@gmail.com>
2020-05-27 04:00:45 +0900

    update abc166_c

--- atcoder/05_AtCoder-Beginner-Contest/abc166/C/main001.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc166/C/main001.cpp
@@ -3,14 +3,17 @@
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-int main() {
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
     int n, m;
     cin >> n >> m;
 
     vector<int> h(n);
+    vector<bool> ok(n, true);
     REP(i, n) cin >> h[i];
 
-    vector<bool> ok(n, true);
     REP(i, m) {
         int a, b;
         cin >> a >> b;
@@ -21,6 +24,7 @@ int main() {
 
     int count = 0;
     REP(i, n) if (ok[i]) count++;
+
     cout << count << '\n';
     return 0;
 }
