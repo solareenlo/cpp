c2c345226c7bbcd21073ead1aa7c498b0f782102
solareenlo <solareenlo@gmail.com>
2020-05-27 04:08:18 +0900

    update abc166_d

--- atcoder/05_AtCoder-Beginner-Contest/abc166/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc166/D/main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <cmath>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long X){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long X;
-    scanf("%lld",&X);
-    solve(X);
+
+    int x;
+    cin >> x;
+
+    int a = 0;
+    int b = 0;
+    for (int i = -150; i <= 150; i++) {
+        for (int j = -150; j <= 150; j++) {
+            if (pow(i, 5) - pow(j, 5) == x) {
+                a = i;
+                b = j;
+            }
+        }
+    }
+
+    cout << a << ' ' << b << '\n';
     return 0;
 }
