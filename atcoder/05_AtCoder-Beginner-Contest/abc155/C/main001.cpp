6739a3a32ce9ab5b4d6822bcae39327cd554fbb9
solareenlo <solareenlo@gmail.com>
2020-05-29 07:54:29 +0900

    add abc155_c

--- atcoder/05_AtCoder-Beginner-Contest/abc155/C/main001.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc155/C/main001.cpp
@@ -3,23 +3,22 @@
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-int main() {
+int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
     int n; cin >> n;
-
-    map<string, int> cnt;
+    map<string, int> count;
     REP(i, n) {
         string s; cin >> s;
-        cnt[s]++;
+        count[s]++;
     }
 
     int maxi = 0;
-    for (auto i : cnt)
+    for (auto i : count)
         maxi = max(maxi, i.second);
 
-    for (auto i : cnt)
+    for (auto i : count)
         if (i.second == maxi)
             cout << i.first << '\n';
     return 0;
