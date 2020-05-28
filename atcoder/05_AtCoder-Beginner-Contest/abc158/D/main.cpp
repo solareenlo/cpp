ea8b278e78258f57b4f2224d2cd65fb8b779b647
solareenlo <solareenlo@gmail.com>
2020-05-29 04:21:16 +0900

    update abc158_d

--- atcoder/05_AtCoder-Beginner-Contest/abc158/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc158/D/main.cpp
@@ -2,30 +2,31 @@
 #include <iostream>
 #include <utility>
 #define REP(i, n) for (int i = 0; i < (n); i++)
-    using namespace std;
+using namespace std;
 
-int main(){
+int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
 
-    string s; cin >> s;
-    int q; cin >> q;
-    string t;
+    string s1; cin >> s1;
+    int n; cin >> n;
+    // 先頭に文字を加える用の文字列
+    // この文字列の後ろに文字を追加するということは，
+    // s1 の先頭に文字を追加したということ．
+    string s2;
 
-    REP(i, q) {
-        int type;
-        cin >> type;
-        if (type == 1) swap(s, t);
+    REP(i, n) {
+        int t; cin >> t;
+        if (t == 1) swap(s1, s2);
         else {
-            int f;
-            char c;
-            cin >> f >> c;
-            if (f == 1) t += c;
-            else s += c;
+            int f; cin >> f;
+            char c; cin >> c;
+            if (f == 1) s2 += c;
+            else s1 += c;
         }
     }
 
-    reverse(t.begin(), t.end());
-    cout << t << s << '\n';
+    reverse(s2.begin(), s2.end());
+    cout << s2 << s1 << endl;
     return 0;
 }
