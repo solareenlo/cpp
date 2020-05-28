3f677d42cc849355fd29820a42e93c20744f2e3e
solareenlo <solareenlo@gmail.com>
2020-05-29 06:02:29 +0900

    add abc157_c

--- atcoder/05_AtCoder-Beginner-Contest/abc157/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc157/C/main.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 #include <map>
-#include <set>
-#include <vector>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long N, long long M, std::vector<long long> s, std::vector<long long> c){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long M;
-    scanf("%lld",&M);
-    std::vector<long long> s(M);
-    std::vector<long long> c(M);
-    for(int i = 0 ; i < M ; i++){
-        scanf("%lld",&s[i]);
-        scanf("%lld",&c[i]);
+
+    int n, m; cin >> n >> m;
+
+    map<int, int> num;
+    REP(i, n) num.insert(make_pair(i + 1, -1));
+
+    bool ans = true;
+    REP(i, m) {
+        int s, c; cin >> s >> c;
+        if (num[s] != -1 && num[s] != c)  ans = false;
+        else num[s] = c;
+        if (n == 3 && s == 1 && c == 0) ans = false;
+        if (n == 2 && s == 1 && c == 0) ans = false;
     }
-    solve(N, M, std::move(s), std::move(c));
+
+    if (ans) {
+        if (n != 1 && num[1] == -1) num[1] = 1;
+        string s;
+        for (auto itr = num.begin(); itr != num.end(); itr++) {
+            if (itr->second == -1) s += '0';
+            else s += itr->second + '0';
+        }
+        cout << stoi(s) << '\n';
+    } else
+        cout << -1 << '\n';
     return 0;
 }
