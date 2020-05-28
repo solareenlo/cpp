19af1a3a9c475908a6ab2affe0de11df74d7ddeb
solareenlo <solareenlo@gmail.com>
2020-05-29 07:17:14 +0900

    add abc156_c

--- atcoder/05_AtCoder-Beginner-Contest/abc156/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc156/C/main.cpp
@@ -1,24 +1,28 @@
+#include <algorithm>
 #include <iostream>
-#include <map>
-#include <set>
+#include <climits>
+#include <cmath>
 #include <vector>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-
-void solve(long long N, std::vector<long long> X){
-
-}
-
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<long long> X(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&X[i]);
+    int n;
+    cin >> n;
+
+    vector<int> x(n);
+    REP(i, n) cin >> x[i];
+    sort(x.begin(), x.end());
+
+    long long mini = LLONG_MAX;
+    for (int i = x[0]; i <= x[n - 1]; i++) {
+        long long sum = 0;
+        REP(j, n) sum += pow(x[j] - i, 2);
+        mini = min(mini, sum);
     }
-    solve(N, std::move(X));
+
+    cout << mini << '\n';
     return 0;
 }
