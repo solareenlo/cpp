72bf46de48a724c9896b0b3c86801af95473f2a8
solareenlo <solareenlo@gmail.com>
2020-05-28 11:24:23 +0900

    add abc161_c

--- atcoder/05_AtCoder-Beginner-Contest/abc161/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc161/C/main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <climits>
+#include <cmath>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long N, long long K){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long K;
-    scanf("%lld",&K);
-    solve(N, K);
+
+    long long n, k;
+    cin >> n >> k;
+
+    long long ans = LLONG_MAX;
+    int count = 0;
+    while (true) {
+        n = min(n % k, abs(n - k));
+        if (ans > n) {
+            ans = n;
+            count = 0;
+        } else
+            count++;
+        if (count == 2) break ;
+    }
+    cout << ans << '\n';
     return 0;
 }
