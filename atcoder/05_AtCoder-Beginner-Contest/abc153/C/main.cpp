a174cc2adea2aee54d7e0108405064aeffdb7f15
solareenlo <solareenlo@gmail.com>
2020-05-29 12:06:26 +0900

    add abc153_c

--- atcoder/05_AtCoder-Beginner-Contest/abc153/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc153/C/main.cpp
@@ -1,26 +1,24 @@
+#include <algorithm>
 #include <iostream>
-#include <map>
-#include <set>
 #include <vector>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-
-void solve(long long N, long long K, std::vector<long long> H){
-
-}
-
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long K;
-    scanf("%lld",&K);
-    std::vector<long long> H(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&H[i]);
-    }
-    solve(N, K, std::move(H));
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> h(n);
+    REP(i, n) cin >> h[i];
+
+    sort(h.begin(), h.end());
+
+    long long sum = 0;
+    REP(i, n - k) sum += h[i];
+
+    cout << sum << '\n';
     return 0;
 }
