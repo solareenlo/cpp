3f566bd1b522630fb82696dc08594480f394e27e
solareenlo <solareenlo@gmail.com>
2020-05-29 13:56:42 +0900

    add abc150_c

--- atcoder/05_AtCoder-Beginner-Contest/abc150/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc150/C/main.cpp
@@ -1,49 +1,51 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
-#include <map>
-#include <numeric>
-#include <set>
 #include <vector>
 #define REP(i, n) for (int i = 0; i < (n); i++)
-#define ALL(vec) (vec).begin(), (vec).end()
-#define SUM(...) accumulate(ALL(__VA_ARGS__),0LL)
-#define DSUM(...) accumulate(ALL(__VA_ARGS__),0.0)
 using namespace std;
-using ll = long long;
-using P = pair<int, int>;
-
-// 便利関数
-template <class T> inline bool chmin(T &a, T b) {if (a > b){a = b;return true;}return false;}
-template <class T> inline bool chmax(T &a, T b) {if (a < b){a = b;return true;}return false;}
-template<class T> inline auto max(const T& a){ return *max_element(ALL(a)); }
-template<class T> inline auto min(const T& a){ return *min_element(ALL(a)); }
-inline ll gcd(ll a,ll b){if(b == 0) return a;return  gcd(b,a%b);}
-inline ll lcm(ll a,ll b){ll g = gcd(a,b);return a / g * b;}
-
-// 出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-
-const int INF = 2002002002;
-
-void solve(long long N, std::vector<long long> P, std::vector<long long> Q){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<long long> P(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&P[i]);
+
+    int n; cin >> n;
+    vector<int> v;
+    for (int i = 1; i <= n; i++)
+        v.push_back(i);
+
+    vector<int> p;
+    REP(i, n) {
+        int tmp;
+        cin >> tmp;
+        p.push_back(tmp);
     }
-    std::vector<long long> Q(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&Q[i]);
+
+    vector<int> q;
+    REP(i, n) {
+        int tmp;
+        cin >> tmp;
+        q.push_back(tmp);
     }
-    solve(N, std::move(P), std::move(Q));
+
+    bool a = false;
+    int countP = 0;
+    bool b = false;
+    int countQ = 0;
+    int count = 0;
+    while (a == false || b == false) {
+        if (v == p) {
+            a = true;
+            countP = count;
+        }
+        if (v == q) {
+            b = true;
+            countQ = count;
+        }
+        next_permutation(v.begin(), v.end());
+        count++;
+    }
+
+    cout << abs(countP - countQ) << '\n';
     return 0;
 }
