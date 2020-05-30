bcfe3461e55a0dfe9903e501ab15290672454429
solareenlo <solareenlo@gmail.com>
2020-05-31 04:36:30 +0900

    add abc142_c

--- atcoder/05_AtCoder-Beginner-Contest/abc142/C/C_001.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc142/C/C_001.cpp
@@ -2,7 +2,7 @@
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-int main() {
+int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
--- atcoder/05_AtCoder-Beginner-Contest/abc142/C/C_002.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc142/C/C_002.cpp
@@ -3,10 +3,8 @@
 using namespace std;
 
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-
-    int n; cin >> n;
+    int n;
+    cin >> n;
 
     vector<pair<int, int> > s;
     REP(i, n) {
