b3bb0d142b7dbd2fafa8877df7fe8aabdb3db12e
solareenlo <solareenlo@gmail.com>
2020-05-31 05:52:59 +0900

    add abc139_c

--- atcoder/05_AtCoder-Beginner-Contest/abc139/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc139/C/main.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long N, std::vector<long long> H){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<long long> H(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&H[i]);
+
+    int n;
+    cin >> n;
+    vector<int> h(n);
+    REP(i, n) cin >> h[i];
+
+    int count = 0;
+    int maxi = 0;
+    REP(i, n - 1) {
+        if (h[i] >= h[i + 1]) count++;
+        else count = 0;
+        maxi = max(maxi, count);
     }
-    solve(N, std::move(H));
+
+    cout << maxi << '\n';
     return 0;
 }
