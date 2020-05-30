b8911ab9d28b41538b751cb44a2bb8fb6da3f8e0
solareenlo <solareenlo@gmail.com>
2020-05-31 06:04:40 +0900

    update abc139_b

--- atcoder/05_AtCoder-Beginner-Contest/abc139/B/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc139/B/main.cpp
@@ -1,20 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(long long A, long long B){
-    if (B == 1) cout << 0 << '\n';
-    else if (A >= B) cout << 1 << '\n';
-    else if ((B-A) % (A-1)) cout << (B-A) / (A-1) + 2 << '\n';
-    else cout << (B-A) / (A-1) + 1 << '\n';
-}
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long A;
-    scanf("%lld",&A);
-    long long B;
-    scanf("%lld",&B);
-    solve(A, B);
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (b == 1) cout << 0 << '\n';
+    else if (a >= b) cout << 1 << '\n';
+    else {
+        int ans = 1;
+        ans += (b - a) / (a - 1);
+        if ((b - a) % (a - 1)) ans++;
+        cout << ans << '\n';
+    }
     return 0;
 }
