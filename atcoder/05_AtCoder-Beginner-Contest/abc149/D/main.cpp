983a9e7e612f73b370add28910b8c7bc3bb925ec
solareenlo <solareenlo@gmail.com>
2020-05-30 13:45:39 +0900

    add abc149_d

--- atcoder/05_AtCoder-Beginner-Contest/abc149/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc149/D/main.cpp
@@ -1,30 +1,39 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long N, long long K, long long R, long long S, long long P, std::string T){
-
-}
+using ll = long long;
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long K;
-    scanf("%lld",&K);
-    long long R;
-    scanf("%lld",&R);
-    long long S;
-    scanf("%lld",&S);
-    long long P;
-    scanf("%lld",&P);
-    std::string T;
-    std::cin >> T;
-    solve(N, K, R, S, P, T);
+
+    int n, k;
+    cin >> n >> k;
+
+    int r, s, p;
+    cin >> r >> s >> p;
+    vector<int> point(128, 0);
+    point['r'] = p;
+    point['s'] = r;
+    point['p'] = s;
+
+    string t;
+    cin >> t;
+
+    vector<vector<ll> > dp(t.size() + 1, vector<ll>(3, 0));
+    vector<bool> flag(100001, false);
+
+    REP(i, n) REP(j, 3) {
+        if (i - k >= 0 && flag[i - k] && t[i] == t[i - k])
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+        else {
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + point[t[i]]);
+            flag[i] = true;
+        }
+    }
+
+    ll ans = 0;
+    REP(i, 3) ans = max(ans, dp[n][i]);
+    cout << ans << '\n';
     return 0;
 }
