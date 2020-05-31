1d22dd7efa55d510d56a467d57efd4ae8111e1bb
solareenlo <solareenlo@gmail.com>
2020-05-31 17:04:49 +0900

    add abc135_c

--- atcoder/05_AtCoder-Beginner-Contest/abc135/C/C_001.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc135/C/C_001.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 using ll = long long;
 
-int main() {
+int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
     int n;
     cin >> n;
+
     vector<int> a(n + 1);
     REP(i, n + 1) cin >> a[i];
+
     vector<int> b(n);
     REP(i, n) cin >> b[i];
 
     ll ans = 0;
-    int tmp = 0;
     REP(i, n) {
         ans += min(a[i], b[i]);
         if (b[i] - a[i] > 0) {
-            tmp = b[i] - a[i];
-            ans += min(tmp, a[i + 1]);
-            a[i + 1] = max(a[i + 1] - tmp, 0);
+            int margin = min(b[i] - a[i], a[i + 1]);
+            ans += margin;
+            a[i + 1] = max(a[i + 1] - margin, 0);
         }
     }
 
