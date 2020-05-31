b2b4b01bf9b7bb4450a4f9507da898d1a9ebd8e9
solareenlo <solareenlo@gmail.com>
2020-05-31 23:01:13 +0900

    add abc169_a_b_c

---
+++ atcoder/05_AtCoder-Beginner-Contest/abc169/C/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll a;
+    cin >> a;
+    long double b;
+    cin >> b;
+    ll ans = a * b;
+    printf("%lld\n", ans);
+    return 0;
+}
---
+++ atcoder/05_AtCoder-Beginner-Contest/abc169/README.md
@@ -0,0 +1,4 @@
+# [AtCoder Beginner Contest 169](https://atcoder.jp/contests/abc169/tasks)
+
+## [B - Multiplication 2](https://atcoder.jp/contests/abc169/tasks/abc169_b)
+- 多倍長整数を扱う問題．
