56ec1a9c8a51bc8df8deb1a47a30b0561f59902c
solareenlo <solareenlo@gmail.com>
2020-06-04 14:10:20 +0900

    add abc094_c

--- atcoder/05_AtCoder-Beginner-Contest/abc093/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc093/C/main.cpp
@@ -1,24 +1,17 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-
-void solve(long long A, long long B, long long C){
-
-}
-
-int main(){
+int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long A;
-    scanf("%lld",&A);
-    long long B;
-    scanf("%lld",&B);
-    long long C;
-    scanf("%lld",&C);
-    solve(A, B, C);
+
+    vector<int> num(3);
+    REP(i, 3) cin >> num[i];
+    sort(num.begin(), num.end());
+
+    int diff = num[2] - num[0] + num[2] - num[1];
+    if (diff % 2) cout << diff / 2 + 2 << '\n';
+    else cout << diff / 2 << '\n';
     return 0;
 }
