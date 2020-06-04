4bd3b3dbf340fd1e21384ab40c8949a851d8d5c4
solareenlo <solareenlo@gmail.com>
2020-06-05 08:09:52 +0900

    add abc090_c

--- atcoder/05_AtCoder-Beginner-Contest/abc090/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc090/C/main.cpp
@@ -1,19 +1,16 @@
-#include <iostream>
-#define REP(i, n) for (int i = 0; i < (n); i++)
+#include <bits/stdc++.h>
 using namespace std;
-
-
-void solve(long long N, long long M){
-
-}
+using ll = long long;
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long M;
-    scanf("%lld",&M);
-    solve(N, M);
+
+    ll n, m;
+    cin >> n >> m;
+
+    if (n == 1 && m > 2) cout << m - 2 << '\n';
+    else if (m == 1 && n > 2) cout << n - 2 << '\n';
+    else cout << (n - 2) * (m - 2) << '\n';
     return 0;
 }
---
+++ atcoder/05_AtCoder-Beginner-Contest/abc090/README.md
@@ -0,0 +1,7 @@
+# [AtCoder Beginner Contest 090](https://atcoder.jp/contests/abc090/tasks)
+
+## [C - Flip,Flip, and Flip......](https://atcoder.jp/contests/abc090/tasks/arc091_a)
+- 1列や1行の場合は，`n - 2` か `m - 2`になる．
+- 2列や2行の場合は，0になる．
+- 3列以上かつ3行以上の場合は，`(n - 2) * (m - 2)` になる．
+- シミュレーションできるコードを書いて確認する．
