0ab3a125c7859fe562c081d03820d16ee657c66e
solareenlo <solareenlo@gmail.com>
2020-06-05 09:06:55 +0900

    add abc089_c

--- atcoder/05_AtCoder-Beginner-Contest/abc089/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc089/C/main.cpp
@@ -1,24 +1,42 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
+using ll = long long;
 
-
-void solve(long long N, std::vector<std::string> S){
-
-}
+ll MARCH[5];
+int P[10] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 2};
+int Q[10] = {1, 1, 1, 2, 2, 3, 2, 2, 3, 3};
+int R[10] = {2, 3, 4, 3, 4, 4, 3, 4, 4, 4};
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<std::string> S(N);
-    for(int i = 0 ; i < N ; i++){
-        std::cin >> S[i];
+
+    int n;
+    cin >> n;
+
+    ll m, a, r, c, h;
+    m = a = r = c = h =0;
+    REP(i, n) {
+        string s;
+        cin >> s;
+        if (s[0] == 'M') m++;
+        if (s[0] == 'A') a++;
+        if (s[0] == 'R') r++;
+        if (s[0] == 'C') c++;
+        if (s[0] == 'H') h++;
     }
-    solve(N, std::move(S));
+
+    MARCH[0] = m;
+    MARCH[1] = a;
+    MARCH[2] = r;
+    MARCH[3] = c;
+    MARCH[4] = h;
+
+    ll ans = 0;
+    REP(i, 10)
+        ans += MARCH[P[i]] * MARCH[Q[i]] * MARCH[R[i]];
+
+    cout << ans << '\n';
     return 0;
 }
---
+++ atcoder/05_AtCoder-Beginner-Contest/abc089/README.md
@@ -0,0 +1,5 @@
+# [AtCoder Beginner Contest 089](https://atcoder.jp/contests/abc089/tasks)
+
+## [C - March](https://atcoder.jp/contests/abc089/tasks/abc089_c)
+- `5C3` が10通りなので，`M`，`A`，`R`，`C`，`H` の個数をそれぞれ数え上げて，
+- 10通りの組合せの総和を計算すれば良い．
