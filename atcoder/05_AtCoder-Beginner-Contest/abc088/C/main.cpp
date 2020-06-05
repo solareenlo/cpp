50ecd2b26a652c4da8ecbdd98bdca24105feecd7
solareenlo <solareenlo@gmail.com>
2020-06-05 09:37:21 +0900

    add abc088_c

--- atcoder/05_AtCoder-Beginner-Contest/abc088/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc088/C/main.cpp
@@ -1,23 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-const string YES = "Yes";
-const string NO = "No";
-
-void solve(std::vector<std::vector<long long>> c){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    std::vector<std::vector<long long>> c(3, std::vector<long long>(3));
-    for(int i = 0 ; i < 3 ; i++){
-        for(int j = 0 ; j < 3 ; j++){
-            scanf("%lld",&c[i][j]);
-        }
+
+    vector<vector<int> > c(3, vector<int>(3, 0));
+    REP(i, 3) REP(j, 3) {
+        cin >> c[i][j];
     }
-    solve(std::move(c));
+
+    bool ans = true;
+    REP(j, 3 - 1) REP(i, 3 - 1)
+        if ((c[i][j] - c[i][j+1]) != (c[i+1][j] - c[i+1][j+1]))
+            ans = false;
+
+    if (ans) cout << "Yes" << '\n';
+    else cout << "No" << '\n';
     return 0;
 }
---
+++ atcoder/05_AtCoder-Beginner-Contest/abc088/README.md
@@ -0,0 +1,4 @@
+# [AtCoder Beginner Contest 088](https://atcoder.jp/contests/abc088/tasks)
+
+## [C - Takahashi's Information](https://atcoder.jp/contests/abc088/tasks/abc088_c)
+-「隣のセルとの差が一定である」という性質を列か行のどちらかを全部確認していく問題．
