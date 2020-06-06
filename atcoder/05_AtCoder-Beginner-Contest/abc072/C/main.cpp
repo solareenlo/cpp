53244e1dd045c8ef313f85e97a2bd3e3c1425e96
solareenlo <solareenlo@gmail.com>
2020-06-06 11:17:09 +0900

    add abc072_c

--- atcoder/05_AtCoder-Beginner-Contest/abc072/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc072/C/main.cpp
@@ -1,24 +1,27 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long N, std::vector<long long> a){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<long long> a(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&a[i]);
+
+    int n;
+    cin >> n;
+    map<int, int> a;
+
+    REP(i, n) {
+        int tmp;
+        cin >> tmp;
+        a[tmp]++;
+        a[tmp - 1]++;
+        a[tmp + 1]++;
     }
-    solve(N, std::move(a));
+
+    int maxi = 0;
+    for(auto i : a)
+        maxi = max(maxi, i.second);
+
+    cout << maxi << '\n';
     return 0;
 }
