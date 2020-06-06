1f102737949967a6595ac9a2e0580f480811f51f
solareenlo <solareenlo@gmail.com>
2020-06-06 10:55:49 +0900

    add abc078_c

--- atcoder/05_AtCoder-Beginner-Contest/abc078/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc078/C/main.cpp
@@ -1,22 +1,14 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long N, long long M){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long M;
-    scanf("%lld",&M);
-    solve(N, M);
+
+    int n, m;
+    cin >> n >> m;
+    int ans = (1900 * m + 100 * (n - m)) * pow(2, m);
+    cout << ans << '\n';
     return 0;
 }
