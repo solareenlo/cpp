9990b898e5b9069207573326a16385d6f144ec96
solareenlo <solareenlo@gmail.com>
2020-06-07 07:56:12 +0900

    add abc058_c

--- atcoder/05_AtCoder-Beginner-Contest/abc058/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc058/C/main.cpp
@@ -1,24 +1,34 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long n, std::vector<std::string> S){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long n;
-    scanf("%lld",&n);
-    std::vector<std::string> S(n);
-    for(int i = 0 ; i < n ; i++){
-        std::cin >> S[i];
+
+    int n;
+    cin >> n;
+
+    vector<multiset<char> > c(n);
+    REP(i, n) {
+        string s;
+        cin >> s;
+        REP(j, s.size())
+            c[i].insert(s[j]);
     }
-    solve(n, std::move(S));
+
+    multiset<char> res = c[0];
+    REP(i, n) {
+        multiset<char> tmp;
+        set_intersection(res.begin(), res.end(),
+                c[i].begin(), c[i].end(),
+                inserter(tmp, tmp.end()));
+        res = tmp;
+    }
+
+    for_each(res.begin(), res.end(), [](char c) {
+            cout << c;
+            });
+    cout << '\n';
     return 0;
 }
