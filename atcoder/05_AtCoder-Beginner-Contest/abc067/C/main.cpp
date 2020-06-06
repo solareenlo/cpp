89e1743065089c0b35ccf02c82cf3fb013f18b1c
solareenlo <solareenlo@gmail.com>
2020-06-06 18:22:32 +0900

    add abc067_c

--- atcoder/05_AtCoder-Beginner-Contest/abc067/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc067/C/main.cpp
@@ -1,21 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(long long N, std::vector<long long> a){
-
-}
+using ll = long long;
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<long long> a(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&a[i]);
-    }
-    solve(N, std::move(a));
+
+    int n;
+    cin >> n;
+
+    vector<ll> a(n);
+    REP(i, n) cin >> a[i];
+
+    vector<ll> sumL(n + 1, 0);
+    REP(i, n) sumL[i + 1] = sumL[i] + a[i];
+
+    reverse(a.begin(), a.end());
+
+    vector<ll> sumR(n + 1, 0);
+    REP(i, n) sumR[i + 1] = sumR[i] + a[i];
+
+    reverse(sumR.begin(), sumR.end());
+
+    ll mini = INT_MAX;
+    for (int i = 1; i < n; i++)
+        mini = min(mini, abs(sumL[i] - sumR[i]));
+
+    cout << mini << '\n';
     return 0;
 }
