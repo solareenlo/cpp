11505f381cb42dd00ec80882fd06d7f9a6575719
solareenlo <solareenlo@gmail.com>
2020-06-06 16:32:17 +0900

    add abc070_c

--- atcoder/05_AtCoder-Beginner-Contest/abc070/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc070/C/main.cpp
@@ -1,45 +1,34 @@
-#include <algorithm>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
-#define ALL(vec) (vec).begin(), (vec).end()
-#define SUM(...) accumulate(ALL(__VA_ARGS__),0LL)
-#define DSUM(...) accumulate(ALL(__VA_ARGS__),0.0)
 using namespace std;
 using ll = long long;
-using P = pair<int, int>;
 
-// 便利関数
-template <class T> inline bool chmin(T &a, T b) {if (a > b){a = b;return true;}return false;}
-template <class T> inline bool chmax(T &a, T b) {if (a < b){a = b;return true;}return false;}
-template<class T> inline auto max(const T& a){ return *max_element(ALL(a)); }
-template<class T> inline auto min(const T& a){ return *min_element(ALL(a)); }
-inline ll gcd(ll a,ll b){if(b == 0) return a;return  gcd(b,a%b);}
-inline ll lcm(ll a,ll b){ll g = gcd(a,b);return a / g * b;}
+template<class T> inline T gcd(T a, T b) {
+    if (b == 0) return a;
+    return  gcd(b, a % b);
+}
 
-// 出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-
-const int INF = 2002002002;
-
-void solve(long long N, std::vector<long long> T){
-
+template<class T> inline T lcm(T a, T b) {
+    return a * b / gcd(a, b);
 }
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<long long> T(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&T[i]);
+
+    int n;
+    cin >> n;
+
+    ll ans;
+    cin >> ans;
+    REP(i, n - 1) {
+        ll tmp;
+        cin >> tmp;
+        ll g = gcd(ans, tmp);
+        ans /= g;
+        ans *= tmp;
     }
-    solve(N, std::move(T));
+
+    cout << ans << '\n';
     return 0;
 }
