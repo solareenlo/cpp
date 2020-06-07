118e2036e03132996dc8e99f9ae6f470e664c89d
solareenlo <solareenlo@gmail.com>
2020-06-07 12:48:33 +0900

    add abc033_c

--- atcoder/05_AtCoder-Beginner-Contest/abc033/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc033/C/main.cpp
@@ -1,20 +1,35 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
-
-void solve(std::string S){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    std::string S;
-    std::cin >> S;
-    solve(S);
+
+    string s;
+    cin >> s;
+
+    int cnt = 0;
+
+    string tmp;
+    REP(i, s.size()) {
+        if (s[i] != '+') {
+            tmp.push_back(s[i]);
+        } else {
+            bool isZero = false;
+            for (int j = 0; j < tmp.size(); j += 2)
+                if (tmp[j] == '0')
+                    isZero = true;
+            if (!(isZero)) cnt++;
+            tmp.clear();
+        }
+    }
+    bool isZero = false;
+    for (int j = 0; j < tmp.size(); j += 2)
+        if (tmp[j] == '0')
+            isZero = true;
+    if (!(isZero)) cnt++;
+
+    cout << cnt << '\n';
     return 0;
 }
