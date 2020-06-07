b208b86284a443eb4bf4b4319aece7b44fa9a558
solareenlo <solareenlo@gmail.com>
2020-06-08 03:13:48 +0900

    add abc011_c

--- atcoder/05_AtCoder-Beginner-Contest/abc011/C/C_001-dp.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc011/C/C_001-dp.cpp
@@ -2,10 +2,12 @@
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-int main() {
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
     int n;
     cin >> n;
-
     int ng1, ng2, ng3;
     cin >> ng1 >> ng2 >> ng3;
 
@@ -17,7 +19,7 @@ int main() {
             if (i == ng1 || i == ng2 || i == ng3) continue ;
             for (int j = 1; j <= 3; j++)
                 if (i - j >= 0)
-                    dp[i - j] = min(dp[i - j], dp[i] + 1);
+                    dp[i - j] = min(dp[i] + 1, dp[i - j]);
         }
         if (dp[0] <= 100) cout << "YES" << '\n';
         else cout << "NO" << '\n';
