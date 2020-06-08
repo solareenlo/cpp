f9aa10aa57201182a8835701ceb223c619c4a118
solareenlo <solareenlo@gmail.com>
2020-06-08 14:36:51 +0900

    add arc003_b

--- atcoder/06_AtCoder-Regular-Contest/arc003/B/main.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc003/B/main.cpp
@@ -1,10 +1,27 @@
 #include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-using ll = long long;
 
 int main() {
     cin.tie(0)->sync_with_stdio(false);
 
+    int n;
+    cin >> n;
+    vector<string> s(n);
+
+    REP(i, n) {
+        string tmp;
+        cin >> tmp;
+        reverse(tmp.begin(), tmp.end());
+        s[i] = tmp;
+    }
+
+    sort(s.begin(), s.end());
+
+    REP(i, n)
+        reverse(s[i].begin(), s[i].end());
+
+    REP(i, n)
+        cout << s[i] << '\n';
     return 0;
 }
