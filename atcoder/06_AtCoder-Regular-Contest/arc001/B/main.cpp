cb300c037efa177b986118b24431497f2382e6f1
solareenlo <solareenlo@gmail.com>
2020-06-08 11:03:03 +0900

    add arc001_b

--- atcoder/06_AtCoder-Regular-Contest/arc001/B/main.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc001/B/main.cpp
@@ -6,5 +6,24 @@ using ll = long long;
 int main() {
     cin.tie(0)->sync_with_stdio(false);
 
+    int a, b;
+    cin >> a >> b;
+
+    int diff = abs(b - a);
+    int div = diff / 10;
+    int rem = diff % 10;
+
+    int ans = div;
+    if (rem == 1) ans += 1;
+    if (rem == 2) ans += 2;
+    if (rem == 3) ans += 3;
+    if (rem == 4) ans += 2;
+    if (rem == 5) ans += 1;
+    if (rem == 6) ans += 2;
+    if (rem == 7) ans += 3;
+    if (rem == 8) ans += 3;
+    if (rem == 9) ans += 2;
+
+    cout << ans << '\n';
     return 0;
 }
