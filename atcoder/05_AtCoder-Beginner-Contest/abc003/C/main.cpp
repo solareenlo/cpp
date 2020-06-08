1475c25bb5aaadba9d2eb3bd8b770b124328afa3
solareenlo <solareenlo@gmail.com>
2020-06-08 09:36:11 +0900

    add abc003_c

--- atcoder/05_AtCoder-Beginner-Contest/abc003/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc003/C/main.cpp
@@ -1,47 +1,23 @@
-#include <algorithm>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
-#define ALL(vec) (vec).begin(), (vec).end()
-#define SUM(...) accumulate(ALL(__VA_ARGS__),0LL)
-#define DSUM(...) accumulate(ALL(__VA_ARGS__),0.0)
 using namespace std;
-using ll = long long;
-using P = pair<int, int>;
-
-// 便利関数
-template <class T> inline bool chmin(T &a, T b) {if (a > b){a = b;return true;}return false;}
-template <class T> inline bool chmax(T &a, T b) {if (a < b){a = b;return true;}return false;}
-template<class T> inline auto max(const T& a){ return *max_element(ALL(a)); }
-template<class T> inline auto min(const T& a){ return *min_element(ALL(a)); }
-inline ll gcd(ll a,ll b){if(b == 0) return a;return  gcd(b,a%b);}
-inline ll lcm(ll a,ll b){ll g = gcd(a,b);return a / g * b;}
-
-// 出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-
-const int INF = 2002002002;
-
-void solve(long long N, long long K, std::vector<long long> R){
-
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long K;
-    scanf("%lld",&K);
-    std::vector<long long> R(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&R[i]);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> r(n);
+    REP(i, n) cin >> r[i];
+    sort(r.begin(), r.end());
+
+    double ans = 0.0;
+    REP(i, k) {
+        ans = (ans + r[n - 1 - (k - 1 - i)]) / 2.0;
     }
-    solve(N, K, std::move(R));
+
+    printf("%.10f\n", ans);
     return 0;
 }
