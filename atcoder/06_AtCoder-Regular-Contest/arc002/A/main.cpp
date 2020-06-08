b2a87abc54b5a63e6af667167642efbf1330da12
solareenlo <solareenlo@gmail.com>
2020-06-08 14:19:21 +0900

    update arc002_a

--- atcoder/06_AtCoder-Regular-Contest/arc002/A/main.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc002/A/main.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-using ll = long long;
 
 int main() {
     cin.tie(0)->sync_with_stdio(false);
@@ -10,18 +9,9 @@ int main() {
     cin >> y;
 
     bool ans = false;
-    if (y % 4 == 0) {
-        ans = true;
-        if (y % 100 == 0) {
-            ans = false;
-            if (y % 400 == 0)
-                ans = true;
-        }
-    } else if (y % 100 == 0) {
-        ans = false;
-        if (y % 400 == 0) ans = true;
-    } else if (y % 400 == 0)
-        ans = true;
+    if (y % 4 == 0) ans = true;
+    if (y % 100 == 0) ans = false;
+    if (y % 400 == 0) ans = true;
 
     if (ans) cout << "YES" << '\n';
     else cout << "NO" << '\n';
