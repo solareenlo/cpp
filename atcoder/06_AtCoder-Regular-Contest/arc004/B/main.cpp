c56f5e08caaec39ac386f73bc1c980d461b4a0e7
solareenlo <solareenlo@gmail.com>
2020-06-08 16:00:05 +0900

    add arc004_b

--- atcoder/06_AtCoder-Regular-Contest/arc004/B/main.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc004/B/main.cpp
@@ -6,5 +6,23 @@ using ll = long long;
 int main() {
     cin.tie(0)->sync_with_stdio(false);
 
+    int n;
+    cin >> n;
+
+    vector<int> d(n);
+    REP(i, n) cin >> d[i];
+
+    sort(d.begin(), d.end());
+
+    int maxi = accumulate(d.begin(), d.end(), 0);
+
+    int mini;
+    if (d.size() == 0) mini = 0;
+    else {
+        mini = max(0, d[n - 1] - (maxi - d[n - 1]));
+    }
+
+    cout << maxi << '\n';
+    cout << mini << '\n';
     return 0;
 }
