7cd10e901e49c98ddddfcd6d8a4351ec7d82e422
solareenlo <solareenlo@gmail.com>
2020-06-11 22:37:49 +0900

    add agc015_b

--- atcoder/07_AtCoder-Grand-Contest/agc015/B/main.cpp
+++ atcoder/07_AtCoder-Grand-Contest/agc015/B/main.cpp
@@ -6,5 +6,21 @@ using ll = long long;
 int main() {
     cin.tie(0)->sync_with_stdio(false);
 
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    ll cnt = 0;
+    REP(i, n) {
+        if (s[i] == 'U') {
+            cnt += n - 1 - i;
+            cnt += i * 2;
+        } else if (s[i] == 'D') {
+            cnt += i;
+            cnt += (n - 1 - i) * 2;
+        }
+    }
+
+    cout << cnt << '\n';
     return 0;
 }
