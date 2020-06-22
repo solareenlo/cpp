090cf49ef99370b695eaae99944af3bd46afe517
solareenlo <solareenlo@gmail.com>
2020-06-22 15:21:35 +0900

    add abc007_c

--- atcoder/05_AtCoder-Beginner-Contest/abc007/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc007/C/main.cpp
@@ -1,36 +1,49 @@
-#include <algorithm>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
-#define ALL(vec) (vec).begin(), (vec).end()
-#define SUM(...) accumulate(ALL(__VA_ARGS__),0LL)
-#define DSUM(...) accumulate(ALL(__VA_ARGS__),0.0)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-// 便利関数
-template <class T> inline bool chmin(T &a, T b) {if (a > b){a = b;return true;}return false;}
-template <class T> inline bool chmax(T &a, T b) {if (a < b){a = b;return true;}return false;}
-template<class T> inline auto max(const T& a){ return *max_element(ALL(a)); }
-template<class T> inline auto min(const T& a){ return *min_element(ALL(a)); }
-inline ll gcd(ll a,ll b){if(b == 0) return a;return  gcd(b,a%b);}
-inline ll lcm(ll a,ll b){ll g = gcd(a,b);return a / g * b;}
-
-// 出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-
-const int INF = 2002002002;
-
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    // Failed to predict input format
+
+    int r, c; cin >> r >> c;
+    int sy, sx; cin >> sy >> sx;
+    sy--; sx--;
+    int gy, gx; cin >> gy >> gx;
+    gy--; gx--;
+    vector<vector<char> > m(r, vector<char>(c));
+    REP(i, r) REP(j, c) cin >> m[i][j];
+
+    vector<vector<int> > d(r, vector<int>(c, -1));
+    d[sy][sx] = 0;
+
+    queue<P> q;
+    q.push(make_pair(sy, sx));
+
+    while (!q.empty()) {
+        P pos = q.front();
+        int y = pos.first;
+        int x = pos.second;
+        q.pop();
+
+        REP(i, 4) {
+            int next_y = y + dy[i];
+            int next_x = x + dx[i];
+            if (next_y < 0 || next_y >= r || next_x < 0 || next_x >= c) continue ;
+            if (m[next_y][next_x] == '#') continue ;
+
+            if (d[next_y][next_x] == -1) {
+                q.push(make_pair(next_y, next_x));
+                d[next_y][next_x] = d[y][x] + 1;
+            }
+        }
+    }
+
+    cout << d[gy][gx] << '\n';
     return 0;
 }
