ece38f31f85b6d46828914715deaa4a7e1819856
solareenlo <solareenlo@gmail.com>
2020-06-23 09:32:40 +0900

    add abc012_d

--- atcoder/05_AtCoder-Beginner-Contest/abc012/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc012/D/main.cpp
@@ -1,35 +1,32 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-// 出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-
-
-void solve(long long N, long long M, std::vector<long long> a, std::vector<long long> b, std::vector<long long> t){
-
-}
-
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long M;
-    scanf("%lld",&M);
-    std::vector<long long> a(M);
-    std::vector<long long> b(M);
-    std::vector<long long> t(M);
-    for(int i = 0 ; i < M ; i++){
-        scanf("%lld",&a[i]);
-        scanf("%lld",&b[i]);
-        scanf("%lld",&t[i]);
-    }
-    solve(N, M, std::move(a), std::move(b), std::move(t));
+    cin.tie(0)->sync_with_stdio(false);
+
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(m), b(m), t(m);
+	REP(i, m) cin >> a[i] >> b[i] >> t[i];
+	REP(i, m) {
+		a[i]--;
+		b[i]--;
+	}
+
+	vector<vector<int> > d(n, vector<int>(n, (int)1e9));
+
+	REP(i, n) d[i][i] = 0;
+	REP(i, m) d[a[i]][b[i]] = d[b[i]][a[i]] = t[i];
+
+	// k が経由する点，i が始点，j が終点
+	REP(k, n) REP(i, n) REP(j, n)
+		d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+
+	int ans = 1e9;
+	REP(i, n)
+		ans = min(ans, *max_element(d[i].begin(), d[i].end()));
+
+	cout << ans << '\n';
     return 0;
 }
