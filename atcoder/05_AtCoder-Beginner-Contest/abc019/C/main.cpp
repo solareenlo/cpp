a2198d272ac826808b0248d772e25cb99dafd584
solareenlo <solareenlo@gmail.com>
2020-06-26 03:59:29 +0900

    add abc019_c

--- atcoder/05_AtCoder-Beginner-Contest/abc019/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc019/C/main.cpp
@@ -1,29 +1,34 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-// 出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-
-
-void solve(long long N, std::vector<long long> a){
-
-}
-
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<long long> a(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&a[i]);
-    }
-    solve(N, std::move(a));
+    cin.tie(0)->sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	map<int, bool> a;
+	REP(i, n) {
+		int tmp;
+		cin >> tmp;
+		a[tmp] = true;
+	}
+
+	int maxi = a.rbegin()->first;
+	for (auto x : a) {
+		if (x.second) {
+			int i = 1;
+			while (x.first * pow(2, i) <= maxi) {
+				if (a.count(x.first * pow(2, i)))
+					a[x.first * pow(2, i)] = false;
+				i++;
+			}
+		}
+	}
+	int cnt = 0;
+	for (auto x : a)
+		if (x.second)
+			cnt++;
+	cout << cnt << '\n';
     return 0;
 }
