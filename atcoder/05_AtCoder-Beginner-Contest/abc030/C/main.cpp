549cd8869f8bb08aaabec630cc5bc4ed41683ade
solareenlo <solareenlo@gmail.com>
2020-06-29 00:53:45 +0900

    add abc030_c

--- atcoder/05_AtCoder-Beginner-Contest/abc030/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc030/C/main.cpp
@@ -1,34 +1,47 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-
-void solve(long long N, long long M, long long X, long long Y, std::vector<long long> a, std::vector<long long> b){
-
-}
-
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long M;
-    scanf("%lld",&M);
-    long long X;
-    scanf("%lld",&X);
-    long long Y;
-    scanf("%lld",&Y);
-    std::vector<long long> a(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&a[i]);
-    }
-    std::vector<long long> b(M);
-    for(int i = 0 ; i < M ; i++){
-        scanf("%lld",&b[i]);
-    }
-    solve(N, M, X, Y, std::move(a), std::move(b));
+    cin.tie(0)->sync_with_stdio(false);
+
+	int n, m; cin >> n >> m;
+	int x, y; cin >> x >> y;
+	queue<int> a, b;
+	REP(i, n) {
+		int t; cin >> t;
+		a.push(t);
+	}
+	REP(i, m) {
+		int t; cin >> t;
+		b.push(t);
+	}
+
+	int cnt = 0;
+	char flag = 'a';
+	while (true) {
+		int A = a.front();
+		int B = b.front();
+		if (flag == 'a') {
+			if (A + x <= B) {
+				cnt++;
+				a.pop();
+				flag = 'b';
+			} else
+				b.pop();
+		} else {
+			if (B + y <= A) {
+				cnt++;
+				b.pop();
+				flag = 'a';
+			} else
+				a.pop();
+		}
+		if (!a.size() || !b.size())
+			break ;
+	}
+
+	if (b.size()) cnt++;
+	cout << cnt / 2 << '\n';
     return 0;
 }
