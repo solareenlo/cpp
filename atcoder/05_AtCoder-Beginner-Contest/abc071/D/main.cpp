4da66f6b96657712bdb0e3c38cad53fa1571d2b5
solareenlo <solareenlo@gmail.com>
2020-07-19 12:47:18 +0900

    add abc071_d

--- atcoder/05_AtCoder-Beginner-Contest/abc071/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc071/D/main.cpp
@@ -1,53 +1,39 @@
-#include <algorithm>
-#include <iomanip>
-#include <iostream>
-#include <numeric>
-#include <vector>
-#define REP(i, n) for (int i = 0; i < (n); i++)
-#define ALL(vec) (vec).begin(), (vec).end()
-#define SUM(...) accumulate(ALL(__VA_ARGS__),0LL)
-#define DSUM(...) accumulate(ALL(__VA_ARGS__),0.0)
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using P = pair<int, int>;
+const ll MOD = 1e9 + 7;
 
-// 便利関数
-template <class T> inline bool chmin(T &a, T b) {if (a > b){a = b;return true;}return false;}
-template <class T> inline bool chmax(T &a, T b) {if (a < b){a = b;return true;}return false;}
-template<class T> inline auto max(const T& a){ return *max_element(ALL(a)); }
-template<class T> inline auto min(const T& a){ return *min_element(ALL(a)); }
-inline ll gcd(ll a,ll b){if(b == 0) return a;return  gcd(b,a%b);}
-inline ll lcm(ll a,ll b){ll g = gcd(a,b);return a / g * b;}
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
 
-// 入出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-#define debug(var)  do{std::cerr << #var << " ↓ "<<"\n";view(var);}while(0);
-#define dbg cerr<<"🥺🥺🥺🥺🥺🥺"<<endl;
-template<typename T> void view(T e){std::cout << e << std::endl;}
-template<typename T> void view(const std::vector<T>& v){for(const auto& e : v){ std::cout << e << " "; } std::cout << std::endl;}
-template<typename T> void view(const std::vector<std::vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
+	int n;
+	cin >> n;
+	string s1, s2;
+	cin >> s1 >> s2;
 
-// 初期時読み込み
-struct initial{initial(){cin.tie(0); ios::sync_with_stdio(0); cout<<fixed<<setprecision(15);};}initial_;
+	ll res;
+	int start;
+	if (s1[0] == s2[0]) {
+		res = 3;
+		start = 1;
+	} else {
+		res = 3 * 2;
+		start = 2;
+	}
+	for (int i = start; i < n; i++) {
+		if (s1[i - 1] == s2[i - 1]) {
+			res *= 2;
+			res %= MOD;
+			if (s1[i] != s2[i]) i++;
+		} else {
+			if (s1[i] != s2[i]) {
+				res *= 3;
+				res %= MOD;
+				i++;
+			}
+		}
+	}
 
-const int INF = 2002002002;
-const long long MOD = 1000000007;
-
-void solve(long long N, std::vector<std::string> S){
-
-}
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<std::string> S(2);
-    for(int i = 0 ; i < 2 ; i++){
-        std::cin >> S[i];
-    }
-    solve(N, std::move(S));
+	cout << res << '\n';
     return 0;
 }
