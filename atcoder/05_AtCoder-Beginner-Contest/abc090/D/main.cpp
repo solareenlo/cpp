f87a7d21a3b7d9cc942429474f8040219736b5bc
solareenlo <solareenlo@gmail.com>
2020-07-29 01:53:44 +0900

    add abc090_d

--- atcoder/05_AtCoder-Beginner-Contest/abc090/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc090/D/main.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
-#define REP(i, n) for (int i = 0; i < (n); i++)
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-
-void solve(long long N, long long K){
-
-}
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long K;
-    scanf("%lld",&K);
-    solve(N, K);
+int main() {
+	ll n, k; cin >> n >> k;
+	ll cnt = 0;
+	if (!k) cnt = n * n;
+	else {
+		for (int b = k + 1; b <= n; b++) {
+			ll div = n / b;
+			ll rem = n % b;
+			cnt += div * max(0LL, b - k) + max(0LL, rem - k + 1);
+		}
+	}
+	cout << cnt << '\n';
     return 0;
 }
