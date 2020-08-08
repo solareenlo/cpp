c5fe680cbbcc51abf22ff644eeaaa48f8a0b9966
solareenlo <solareenlo@gmail.com>
2020-08-08 18:33:41 +0900

    add arc041_b

--- atcoder/06_AtCoder-Regular-Contest/arc041/B/main2.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc041/B/main2.cpp
@@ -6,7 +6,7 @@ const int dx[4] = {0, 1, 0,-1};
 const int dy[4] = {-1,0, 1, 0};
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
+	cin.tie(0)->sync_with_stdio(false);
 
 	int n, m; cin >> n >> m;
 	vector<vector<int> > b(n, vector<int>(m, 0));
@@ -16,20 +16,16 @@ int main() {
 	}
 
 	vector<vector<int> > res(n, vector<int>(m, 0));
-	for (int i = 0; i < n - 1; i++) {
+	for (int i = 1; i < n - 1; i++) {
 		for (int j = 1; j < m - 1; j++) {
-			res[i+1][j] = b[i][j];
-			int tmp = b[i][j];
-			REP(k, 4)
-				if (i + 1 + dy[k] >= 0 && i + 1 + dy[k] < n)
-					if (j + dx[k] >= 0 && j + dx[k] < m)
-						b[i+1+dy[k]][j+dx[k]] -= tmp;
+			REP(k, 4) res[i+dy[k]][j+dx[k]] += b[i][j];
 		}
 	}
 
+	cout << '\n';
 	REP(i, n) {
 		REP(j, m) cout << res[i][j];
 		cout << '\n';
 	}
-    return 0;
+	return 0;
 }
