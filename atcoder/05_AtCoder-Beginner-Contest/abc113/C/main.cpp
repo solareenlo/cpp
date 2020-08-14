d9365caea50b79ec1ea95a32c0d72febbf0c0314
solareenlo <solareenlo@gmail.com>
2020-08-15 04:58:59 +0900

    add abc113_c

--- atcoder/05_AtCoder-Beginner-Contest/abc113/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc113/C/main.cpp
@@ -1,28 +1,24 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
 
-void solve(long long N, long long M, std::vector<long long> P, std::vector<long long> Y){
+	int n, m; cin >> n >> m;
+	vector<int> p(m), y(m);
+	vector<vector<int> > num(n + 1);
+	REP(i, m) {
+		cin >> p[i] >> y[i];
+		num[p[i]].push_back(y[i]);
+	}
 
-}
+	REP(i, n)
+		sort(num[i + 1].begin(), num[i + 1].end());
 
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long M;
-    scanf("%lld",&M);
-    std::vector<long long> P(M);
-    std::vector<long long> Y(M);
-    for(int i = 0 ; i < M ; i++){
-        scanf("%lld",&P[i]);
-        scanf("%lld",&Y[i]);
-    }
-    solve(N, M, std::move(P), std::move(Y));
-    return 0;
+	REP(i, m) {
+		int j = lower_bound(num[p[i]].begin(), num[p[i]].end(), y[i]) - num[p[i]].begin();
+		printf("%06d%06d\n", p[i], j + 1);
+	}
+	return 0;
 }
