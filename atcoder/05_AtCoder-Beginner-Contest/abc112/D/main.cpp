9e9eed449167aa4f10179a320d78fe52a797538d
solareenlo <solareenlo@gmail.com>
2020-08-15 03:18:53 +0900

    add abc112_d

--- atcoder/05_AtCoder-Beginner-Contest/abc112/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc112/D/main.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
 
-void solve(long long N, long long M){
+	int n, m; cin >> n >> m;
 
-}
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long M;
-    scanf("%lld",&M);
-    solve(N, M);
+	int ret = 0;
+	if (n == 1) ret = m;
+	else if (n == m ) ret = 1;
+	else {
+		int div = m / n;
+		for (int i = 1; i <= div; i++) {
+			int mod = (m - i * n) % i;
+			if (mod == 0) ret = i;
+		}
+	}
+	cout << ret << '\n';
     return 0;
 }
--- atcoder/05_AtCoder-Beginner-Contest/abc112/README.md
+++ atcoder/05_AtCoder-Beginner-Contest/abc112/README.md
@@ -1,5 +1,10 @@
 # [AtCoder Beginner Contest 112](https://atcoder.jp/contests/abc112)
 
+## [D - Partition](https://atcoder.jp/contests/abc112/tasks/abc112_d)
+- `a[1]`, `a[2]`, `a[3]`, ... , `a[N]` の最大公約数を `D` とすると，
+- 条件より，`M - D*N` が `D` で割り切れないといけない．
+- `1 ~ M/N` の間の数 `i` で `(M - i * N) % i == 0` となるような最大の `i` を見つけると，それが答え．
+
 ## [C - Pyramid](https://atcoder.jp/contests/abc112/tasks/abc112_c)
 - 101 * 101 グリッドでセンターがどこに位置するのかを 全探索 すれば良い．
 - まずは，`仮の中心座標`と`与えられた座標`と`そこの高さ`から，`仮の中心の高さ`を算出する．
