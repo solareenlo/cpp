f20745adbccbaf7894ff5b3339cae5e4c88c956a
solareenlo <solareenlo@gmail.com>
2020-08-19 04:45:15 +0900

    update 公約数のリスト

--- atcoder/tools/common-divisor-list.cpp
+++ atcoder/tools/common-divisor-list.cpp
@@ -3,9 +3,10 @@
 #include <set>
 using namespace std;
 
-set<int> divisorList(int n) {
-    set<int> div;
-    for (int i = 1; i <= n; i++) {
+template<class T>
+set<T> divisorList(T n) {
+    set<T> div;
+    for (T i = 1; i <= n; i++) {
         if (n % i == 0){
             div.insert(i);
             if (i * i != n)
@@ -15,10 +16,11 @@ set<int> divisorList(int n) {
     return div;
 }
 
-set<int> commonDivList(int a, int b) {
-    set<int> aDiv = divisorList(a);
-    set<int> bDiv = divisorList(b);
-    set<int> res;
+template<class T>
+set<T> commonDivList(T a, T b) {
+    set<T> aDiv = divisorList(a);
+    set<T> bDiv = divisorList(b);
+    set<T> res;
     set_intersection(aDiv.begin(), aDiv.end(),
             bDiv.begin(), bDiv.end(),
             inserter(res, res.end()));
