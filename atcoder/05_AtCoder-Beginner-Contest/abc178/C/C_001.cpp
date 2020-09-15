ae0da34f33e8f32a115dfb45fe7d35226379bc5d
solareenlo <solareenlo@gmail.com>
2020-09-15 11:41:20 +0900

    add abc178_e

--- atcoder/05_AtCoder-Beginner-Contest/abc178/C/C_001.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc178/C/C_001.cpp
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
-#define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 using ll = long long;
 const ll MOD = 1e9 + 7;
 
-inline ll powmod(ll x, ll y) {
-	ll res = 1;
-	while (y--)
-		res = res * x % MOD;
-	return res;
-}
-
 int main() {
 	ll n; cin >> n;
-	ll res = powmod(10, n) - powmod(9, n) - powmod(9, n) + powmod(8, n);
-	res %= MOD;
-	res = (res + MOD) % MOD;
-	cout << res << '\n';
-    return 0;
+	ll a, b, c;
+	a = b = c = 1;
+	while (n--) {
+		a = a * 10 % MOD;
+		b = b * 9 % MOD;
+		c = c * 8 % MOD;
+	}
+	cout << (((a - 2*b + c) % MOD) + MOD) % MOD << '\n';
+	return 0;
 }
--- atcoder/05_AtCoder-Beginner-Contest/abc178/D_001.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#define REP(i, n) for (int i = 0; i < (n); i++)
-using namespace std;
-
-int main() {
-	cin.tie(0)->sync_with_stdio(false);
-
-	return 0;
-}
