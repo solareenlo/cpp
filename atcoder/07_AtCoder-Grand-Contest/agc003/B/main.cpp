d6a070ac4891bddc36f969326202f02ffb313e73
solareenlo <solareenlo@gmail.com>
2020-09-24 03:09:04 +0900

    add agc003_b

--- atcoder/07_AtCoder-Grand-Contest/agc003/B/main.cpp
+++ atcoder/07_AtCoder-Grand-Contest/agc003/B/main.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
-#define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 using ll = long long;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
+	cin.tie(0)->sync_with_stdio(false);
 
-    return 0;
+	int n; cin >> n;
+	ll res, rem;
+	res = rem = 0;
+	while (n--) {
+		ll a; cin >> a;
+		if (a && rem) {
+			res++;
+			a--;
+		}
+		res += a / 2;
+		rem = a % 2;
+	}
+	cout << res << '\n';
+	return 0;
 }
