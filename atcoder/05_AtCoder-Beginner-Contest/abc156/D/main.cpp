c5b078abbe25bf78671d928965551e8f30688235
solareenlo <solareenlo@gmail.com>
2020-10-04 03:32:23 +0900

    add abc156_d

--- atcoder/05_AtCoder-Beginner-Contest/abc156/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc156/D/main.cpp
@@ -1,25 +1,35 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
 
-const long long MOD = 1000000007;
+inline ll modpow(ll a, ll n) {
+	ll res = 1;
+	while (n) {
+		if (n & 1) res = res * a % MOD;
+		a = a * a % MOD;
+		n >>= 1;
+	}
+	return res;
+}
 
-void solve(long long n, long long a, long long b){
-
+inline ll nCr(ll n, ll r) {
+	ll x = 1;
+	ll y = 1;
+	REP(i, r) {
+		x = (x * (n - i)) % MOD;
+		y = (y * (r - i)) % MOD;
+	}
+	return x * modpow(y, MOD - 2) % MOD;
 }
 
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long n;
-    scanf("%lld",&n);
-    long long a;
-    scanf("%lld",&a);
-    long long b;
-    scanf("%lld",&b);
-    solve(n, a, b);
+	ll n, a, b; cin >> n >> a >> b;
+	ll res = modpow(2, n) - 1 - nCr(n, a) - nCr(n, b);
+	res %= MOD;
+	res += MOD;
+	res %= MOD;
+	cout << res << '\n';
     return 0;
 }
--- atcoder/05_AtCoder-Beginner-Contest/abc156/README.md
+++ atcoder/05_AtCoder-Beginner-Contest/abc156/README.md
@@ -1,1 +1,4 @@
-# [AtCoder Beginner Contest 156](https://atcoder.jp/contests/abc156/tasks)
+# [AtCoder Beginner Contest 156](https://atcoder.jp/contests/abc156)
+
+## [D - Bouquet](https://atcoder.jp/contests/abc156/tasks/abc156_d)
+- 解説は Notion にあり．
