c6b2b6260c5441a096f87ebb2e6cc556e32dde4b
solareenlo <solareenlo@gmail.com>
2020-10-10 15:11:16 +0900

    add abc132_d

--- atcoder/05_AtCoder-Beginner-Contest/abc132/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc132/D/main.cpp
@@ -1,23 +1,19 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
+const int MOD = 1000000007;
+int64_t C[2002][2002];
 
-const long long MOD = 1000000007;
-
-void solve(long long N, long long K){
-
-}
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    long long K;
-    scanf("%lld",&K);
-    solve(N, K);
-    return 0;
+int main() {
+	int n, k; cin >> n >> k;
+	C[0][0] = 1;
+	for (int i = 1; i <= n; i++) {
+		REP(j, i+1) {
+			if (j == 0) C[i][j] = 1;
+			else C[i][j] = (C[i-1][j] + C[i-1][j-1]) % MOD;
+		}
+	}
+	REP(i, k)
+		cout << (C[n-k+1][i+1] * C[k-1][i]) % MOD << '\n';
+	return 0;
 }
