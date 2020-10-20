853f7633c8b90b5b6926834bc902a6ad5e2b4d2a
solareenlo <solareenlo@gmail.com>
2020-10-20 17:10:44 +0900

    add abc176_d

--- atcoder/05_AtCoder-Beginner-Contest/abc176/D/D_002.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc176/D/D_002.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
+#define REP(i, n) for (int i = -2; i < (n); i++)
 using namespace std;
+
 int main() {
 	int H, W, Ch, Cw, Dh, Dw;
 	cin >> H >> W >> Ch >> Cw >> Dh >> Dw;
+	Ch--, Cw--, Dh--, Dw--;
 	vector<string> s(H);
 	for (int i = 0; i < H; i++) cin >> s[i];
 
 	vector<vector<int> > dist(H, vector<int>(W, -1));
-	deque<tuple<int, int, int> > dq;
-	dq.emplace_front(--Ch, --Cw, 0);
+	deque<tuple<int, int, int> > dq; // <h, w, d>
+	dq.emplace_front(Ch, Cw, 0);
 	while (!dq.empty()) {
 		int h, w, d;
 		tie(h, w, d) = dq.front();
@@ -17,10 +20,11 @@ int main() {
 		if (s[h][w] == '#') continue ;
 		if (dist[h][w] >= 0) continue ;
 		dist[h][w] = d;
-		for (int dh = -2; dh <= 2; dh++) for (int dw = -2; dw <= 2; dw++)
+		REP(dh, 3) REP(dw, 3) {
 			if (dh*dh + dw*dw == 1) dq.emplace_front(h+dh, w+dw, d);
 			else if (dh*dh + dw*dw > 1) dq.emplace_back(h+dh, w+dw, d+1);
+		}
 	}
-	cout << dist[--Dh][--Dw] << '\n';
+	cout << dist[Dh][Dw] << '\n';
 	return 0;
 }
--- atcoder/05_AtCoder-Beginner-Contest/abc176/README.md
+++ atcoder/05_AtCoder-Beginner-Contest/abc176/README.md
@@ -2,3 +2,6 @@
 
 ## [E - Bomber](https://atcoder.jp/contests/abc176/tasks/abc176_e)
 - 解説は Notion にあり．
+
+## [D - Wizard in Maze](https://atcoder.jp/contests/abc176/tasks/abc176_d)
+- 解説は Notion に有り．
