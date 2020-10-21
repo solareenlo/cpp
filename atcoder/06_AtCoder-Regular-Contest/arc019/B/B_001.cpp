8e72921ca5e6e60552cd99d57dda6885b5f7e04a
solareenlo <solareenlo@gmail.com>
2020-10-21 19:06:29 +0900

    add arc019_b

--- atcoder/06_AtCoder-Regular-Contest/arc019/B/B_001.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc019/B/B_001.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
 int main() {
 	string s; cin >> s;
 	int n = s.size();
 	int cnt = 0;
-	for (int i = 0; i < n / 2; i++)
-		cnt += (s[i] != s[n - 1 - i]);
-	int res = 25 * n;
+	REP(i, n / 2)
+		cnt += s[i] != s[n - 1 - i];
+	int res = n * 25;
+	if (n % 2 && cnt == 0)
+		res -= 25;
 	if (cnt == 1)
 		res -= 2;
-	if (cnt == 0 && n % 2)
-		res -= 25;
 	cout << res << '\n';
-	return 0;
+    return 0;
 }
