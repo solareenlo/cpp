46e145ceaa9d112e24a95239c0e7e88fd163dc86
solareenlo <solareenlo@gmail.com>
2020-10-22 07:50:30 +0900

    add abc125_c

--- atcoder/05_AtCoder-Beginner-Contest/abc125/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc125/C/main.cpp
@@ -1,24 +1,23 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-
-void solve(long long N, std::vector<long long> A){
-
+inline int ft_gcd(int x, int y) {
+	return (!y) ? x : gcd(y, x % y);
 }
 
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long N;
-    scanf("%lld",&N);
-    std::vector<long long> A(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&A[i]);
-    }
-    solve(N, std::move(A));
-    return 0;
+int main() {
+	int n; cin >> n;
+	vector<int> a(n);
+	REP(i, n) cin >> a[i];
+	vector<int> l(n), r(n);
+	REP(i, n-1)
+		l[i+1] = ft_gcd(l[i], a[i]);
+	for (int i = n-1; i > 0; i--)
+		r[i-1] = ft_gcd(r[i], a[i]);
+	int res = 1;
+	REP(i, n)
+		res = max(res, ft_gcd(l[i], r[i]));
+	cout << res << '\n';
+	return 0;
 }
--- atcoder/05_AtCoder-Beginner-Contest/abc125/README.md
+++ atcoder/05_AtCoder-Beginner-Contest/abc125/README.md
@@ -7,3 +7,6 @@
 - マイナスの個数が偶数の場合
 	- 操作を繰り返していくと，全ての符号をプラスにすることができる．
 	- ので，答えは，全ての数の絶対値の総和．
+
+## [C - GCD on Blackboard](https://atcoder.jp/contests/abc125/tasks/abc125_c)
+- 解説は Notion に有り．
