704963f81d76efc663f4d7dbbd4d1479a90b440a
solareenlo <solareenlo@gmail.com>
2020-10-22 08:02:01 +0900

    update abc147_b

--- atcoder/05_AtCoder-Beginner-Contest/abc147/B/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc147/B/main.cpp
@@ -1,26 +1,12 @@
-#include <algorithm>
-#include <iostream>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-void solve(std::string S){
-    int n;
-    if (S.size() % 2) n = S.size()/2 + 1;
-    else n = S.size()/2;
-    string s1 = S.substr(0, n);
-    string s2 = S.substr(n);
-    reverse(s2.begin(), s2.end());
-    int count = 0;
-    REP(i, S.size()/2)
-        if (s1[i] != s2[i]) count++;
-    cout << count << '\n';
-}
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    std::string S;
-    std::cin >> S;
-    solve(S);
-    return 0;
+int main() {
+	string s; cin >> s;
+	int n = s.size();
+	int cnt = 0;
+	REP(i, n/2) cnt += (s[i] != s[n - 1 - i]);
+	cout << cnt << '\n';
+	return 0;
 }
