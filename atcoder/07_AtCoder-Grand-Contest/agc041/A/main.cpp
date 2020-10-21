b8c3fb33ee3ebb4245a538bc901910e2a1054747
solareenlo <solareenlo@gmail.com>
2020-10-22 08:40:48 +0900

    update agc041_a

--- atcoder/07_AtCoder-Grand-Contest/agc041/A/main.cpp
+++ atcoder/07_AtCoder-Grand-Contest/agc041/A/main.cpp
@@ -1,17 +1,14 @@
 #include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 using ll = long long;
 
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
-
-    ll n, a, b;
-    cin >> n >> a >> b;
-
-    ll ans = 0;
-    if ((b - a) % 2) ans = min(n - a, b - 1) - (b - a - 1) / 2;
-    else ans = (b - a) / 2;
-
-    cout << ans << '\n';
-    return 0;
+	ll n, a, b; cin >> n >> a >> b;
+	ll res = 0;
+	if (b - a == 1) res = min(n - a, b - 1);
+	else if ((b - a) % 2) res = min(n - a, b - 1) - (b - a - 1)/2;
+	else res = (b - a) / 2;
+	cout << res << '\n';
+	return 0;
 }
