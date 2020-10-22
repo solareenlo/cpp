d3aee8673f507c3bf8dbc2863ba7f07f1093f1f1
solareenlo <solareenlo@gmail.com>
2020-10-23 00:57:22 +0900

    update arc003_b and arc105_a

--- atcoder/06_AtCoder-Regular-Contest/arc003/B/main.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc003/B/main.cpp
@@ -1,27 +1,13 @@
 #include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-
 int main() {
-    cin.tie(0)->sync_with_stdio(false);
-
-    int n;
-    cin >> n;
-    vector<string> s(n);
-
-    REP(i, n) {
-        string tmp;
-        cin >> tmp;
-        reverse(tmp.begin(), tmp.end());
-        s[i] = tmp;
-    }
-
-    sort(s.begin(), s.end());
-
-    REP(i, n)
-        reverse(s[i].begin(), s[i].end());
-
-    REP(i, n)
-        cout << s[i] << '\n';
-    return 0;
+	int n; cin >> n;
+	vector<string> s(n);
+	REP(i, n) cin >> s[i];
+	REP(i, n) reverse(s[i].begin(), s[i].end());
+	sort(s.begin(), s.end());
+	REP(i, n) reverse(s[i].begin(), s[i].end());
+	REP(i, n) cout << s[i] << '\n';
+	return 0;
 }
--- atcoder/06_AtCoder-Regular-Contest/arc105/A/main.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc105/A/main.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
 	int a, b, c, d; cin >> a >> b >> c >> d;
-	int sum = a + b + c + d;
+	int sum  = a + b + c + d;
 	bool ok = false;
-	if (a == sum - a) ok = true;
-	if (b == sum - b) ok = true;
-	if (c == sum - c) ok = true;
-	if (d == sum - d) ok = true;
-	if (a+b == c+d) ok = true;
-	if (a+c == b+d) ok = true;
-	if (a+d == b+c) ok = true;
+	if (sum - a == a) ok = true;
+	if (sum - b == b) ok = true;
+	if (sum - c == c) ok = true;
+	if (sum - d == d) ok = true;
+	if (c + d == a + b) ok = true;
+	if (b + d == a + c) ok = true;
+	if (b + c == a + d) ok = true;
 	cout << (ok ? "Yes" : "No") << '\n';
-    return 0;
+	return 0;
 }
