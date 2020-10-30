72d80797ff899f97b5e65599a583f08cca490040
solareenlo <solareenlo@gmail.com>
2020-10-31 01:29:51 +0900

    add abc089_d

--- atcoder/05_AtCoder-Beginner-Contest/abc089/D/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc089/D/main.cpp
@@ -1,38 +1,24 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
+int h, w, d, a, q, l, r;
+int main() {
+	cin >> h >> w >> d;
+	vector<int> x(h * w + 1, 0), y(h * w + 1, 0);
+	REP(i, h) REP(j, w) {
+		cin >> a;
+		x[a] = j;
+		y[a] = i;
+	}
 
+	vector<int> s(h * w + 1, 0);
+	REP(i, h * w - d + 1)
+		s[i+d] = s[i] + abs(x[i+d] - x[i]) + abs(y[i+d] - y[i]);
 
-void solve(long long H, long long W, long long D, std::vector<std::vector<long long>> A, long long Q, std::vector<long long> L, std::vector<long long> R){
-
-}
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long H;
-    scanf("%lld",&H);
-    long long W;
-    scanf("%lld",&W);
-    long long D;
-    scanf("%lld",&D);
-    std::vector<std::vector<long long>> A(H, std::vector<long long>(W));
-    for(int i = 0 ; i < H ; i++){
-        for(int j = 0 ; j < W ; j++){
-            scanf("%lld",&A[i][j]);
-        }
-    }
-    long long Q;
-    scanf("%lld",&Q);
-    std::vector<long long> L(Q);
-    std::vector<long long> R(Q);
-    for(int i = 0 ; i < Q ; i++){
-        scanf("%lld",&L[i]);
-        scanf("%lld",&R[i]);
-    }
-    solve(H, W, D, std::move(A), Q, std::move(L), std::move(R));
-    return 0;
+	cin >> q;
+	while (q--) {
+		cin >> l >> r;
+		cout << s[r] - s[l] << '\n';
+	}
+	return 0;
 }
--- atcoder/05_AtCoder-Beginner-Contest/abc089/README.md
+++ atcoder/05_AtCoder-Beginner-Contest/abc089/README.md
@@ -1,4 +1,7 @@
-# [AtCoder Beginner Contest 089](https://atcoder.jp/contests/abc089/tasks)
+# [AtCoder Beginner Contest 089](https://atcoder.jp/contests/abc089)
+
+## [D - Practical Skill Test](https://atcoder.jp/contests/abc089/tasks/abc089_d)
+- 解説は Notion に有り．
 
 ## [C - March](https://atcoder.jp/contests/abc089/tasks/abc089_c)
 - `5C3` が10通りなので，`M`，`A`，`R`，`C`，`H` の個数をそれぞれ数え上げて，
