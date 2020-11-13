f2daffdc52a32ed62332436a55f0b3cad8424cdb
solareenlo <solareenlo@gmail.com>
2020-11-14 03:22:17 +0900

    add abc014_c

--- atcoder/05_AtCoder-Beginner-Contest/abc014/C/main.cpp
+++ atcoder/05_AtCoder-Beginner-Contest/abc014/C/main.cpp
@@ -1,31 +1,18 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 
-// 出力
-void print() { std::cout << '\n'; }
-template <class T>void print(const T &x) {std::cout << x <<'\n';}
-template <class T, class... Args>void print(const T &x, const Args &... args) {std::cout << x << " ";print(args...);}
-
-
-void solve(long long n, std::vector<long long> a, std::vector<long long> b){
-
-}
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    long long n;
-    scanf("%lld",&n);
-    std::vector<long long> a(n);
-    std::vector<long long> b(n);
-    for(int i = 0 ; i < n ; i++){
-        scanf("%lld",&a[i]);
-        scanf("%lld",&b[i]);
-    }
-    solve(n, std::move(a), std::move(b));
+int main() {
+	int n; cin >> n;
+	vector<int> v(1000001, 0);
+	REP(i, n) {
+		int l, r; cin >> l >> r;
+		v[l]++;
+		v[r+1]--;
+	}
+	vector<int> s(1000002, 0);
+	REP(i, 1000002)
+		s[i+1] = s[i] + v[i];
+	cout << *max_element(s.begin(), s.end()) << '\n';
     return 0;
 }
