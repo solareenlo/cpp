8928f8cf5271d59dcd45659617fdfd7b00241ff4
solareenlo <solareenlo@gmail.com>
2020-11-21 19:31:17 +0900

    Add: arc107_c

--- atcoder/06_AtCoder-Regular-Contest/arc107/C/C_001.cpp
+++ atcoder/06_AtCoder-Regular-Contest/arc107/C/C_001.cpp
@@ -7,25 +7,27 @@ using namespace atcoder;
 
 int main() {
 	int n, K; cin >> n >> K;
-	int a[n][n];
+
+	vector<vector<int> > a(n, vector<int>(n));
 	REP(i, n) REP(j, n) cin >> a[i][j];
+
 	dsu x(n), y(n);
 	REP(i, n) REP(j, i) {
 		bool ok = true;
 		REP(k, n) if (a[k][i] + a[k][j] > K) ok = false;
 		if (ok) x.merge(i, j);
 		ok = true;
-		REP(k, n) if (a[i][k] + a[j][k] > K) ok = false;
+		REP(k, n) if (a[j][k] + a[i][k] > K) ok = false;
 		if (ok) y.merge(i, j);
 	}
-	mint f[n+1];
-	f[0] = 1;
+
+	vector<mint> f(n+1, 1);
 	REP(i, n) f[i+1] = f[i] * (i+1);
 	mint res = 1;
 	auto xv = x.groups();
 	for (auto x : xv) res *= f[x.size()];
 	auto yv = y.groups();
-	for (auto y : yv) res *= f[y.size()];
+	for (auto y :yv) res *= f[y.size()];
 	cout << res.val() << '\n';
-	return 0;
+    return 0;
 }
